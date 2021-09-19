2772e91c5ab613420e0bf2dde0961832267ad476
figiter771 <35864921+figiter771@users.noreply.github.com>
2021-09-19 23:43:20 +0300

    Initial commit

---
+++ src/Main/Main.tsx
@@ -0,0 +1,39 @@
+import camel from '../pictures/camel.jpg';
+import banner from '../pictures/1.jpg'
+import img1 from '../pictures/2.jpg'
+import img2 from '../pictures/3.jpg'
+import img3 from '../pictures/4.jpg'
+import img4 from '../pictures/5.jpg'
+
+import { AlignedBlock, BannerImage, BlueButton, ButtonBlock, CenteredBlock, CenteredImage, CorrectText, FullImage, LeftImage, MisalignedBlock, RightImage, SpacingBlock, StylizedText } from './MainStyles';
+export default function Main() {
+
+    return (
+        <div>
+            <CorrectText>Programmatūras testēšanas pamati</CorrectText>
+            <BannerImage src={banner} alt="camel" />
+
+            <MisalignedBlock>
+
+                <LeftImage src={img1} alt="..." />
+                <SpacingBlock />
+                <RightImage src={img2} alt="..." />
+
+            </MisalignedBlock>
+            <ButtonBlock>
+                <BlueButton>Uz kursa materiāliem</BlueButton>
+            </ButtonBlock>
+            <AlignedBlock>
+                <CenteredImage alt="programmaturas izstrades veicamie uzdevumi" />
+                <CenteredBlock>
+                    <FullImage src={img3} alt="..." />
+                    <StylizedText>
+                        Piemērs
+                    </StylizedText>
+                </CenteredBlock>
+
+            </AlignedBlock>
+
+        </div>
+    )
+}
---
+++ src/Main/MainStyles.tsx
@@ -0,0 +1,148 @@
+import styled from 'styled-components';
+
+export const BannerImage = styled.img`
+  width:80%;
+  align-self: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  display:block;
+
+
+`
+export const MisalignedBlock = styled.div`
+width:85%;
+margin-left: 5%;
+margin-right: 10%;
+margin-bottom: 10px;
+margin-top: 10px;
+display:flex;
+
+`
+export const CenteredImage = styled.img`
+  width:45%;
+  align-self: center;
+  margin-left: auto;
+  margin-right: auto;
+  display:block;
+
+`
+export const FullImage = styled.img`
+  width:100%;
+  display:block;
+
+`
+
+export const CenteredBlock = styled.div`
+  width:45%;
+  align-self: center;
+  margin-left: auto;
+  margin-right: auto;
+  display:block;
+
+`
+
+export const AlignedBlock = styled.div`
+width:80%;
+align-self: center;
+margin-left: auto;
+margin-right: auto;
+margin-bottom: 10px;
+margin-top: 10px;
+display:flex;
+
+`
+
+export const ButtonBlock = styled.div`
+justify-content: center;
+align-items: center;
+display:flex;
+
+`
+
+export const StylizedText = styled.p`
+font-family: "Comic Sans MS", "Comic Sans", cursive;
+justify-content: center;
+align-items: center;
+display:flex;
+
+
+`
+export const BlueButton = styled.button`
+  align-items: center;
+  background-clip: padding-box;
+  background-color: #fa6400;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: system-ui,-apple-system,system-ui,"Helvetica Neue",Helvetica,Arial,sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  justify-content: center;
+  line-height: 1.25;
+  margin: 0;
+  min-height: 3rem;
+  padding: calc(.875rem - 1px) calc(1.5rem - 1px);
+  position: relative;
+  text-decoration: none;
+  transition: all 250ms;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: baseline;
+  width: auto;
+
+
+&:hover,
+focus {
+  background-color: #fb8332;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
+}
+
+&:hover {
+  transform: translateY(-1px);
+}
+
+&:active {
+  background-color: #c85000;
+  box-shadow: rgba(0, 0, 0, .06) 0 2px 4px;
+  transform: translateY(0);
+}
+`
+
+export const LeftImage = styled.img`
+  width:42.35%;
+  align-self: center;
+  margin-left: auto;
+  margin-right: auto;
+  display:block;
+
+`
+export const RightImage = styled.img`
+  width:42.35%;
+  align-self: center;
+  margin-left: auto;
+  margin-right: auto;
+  display:block;
+
+`
+export const SpacingBlock = styled.p`
+  width:5.88%;
+  display:block;
+
+`
+export const CorrectText = styled.h1`
+font-family: "Times New Roman", Times, serif;
+letter-spacing: 2px;
+font-weight: bold;
+font-variant: small-caps;
+justify-content: center;
+align-items: center;
+display:flex;
+
+`
